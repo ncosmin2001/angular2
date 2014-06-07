198d79079d0b3a13435d59f9977ce065060a9734
marian radu <doradu@pentalog.fr>
2014-06-07 12:03:00 +0300

    skills

---
+++ app/php/models/skill.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: doradu
+ * Date: 6/7/14
+ * Time: 10:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class skillModel {
+    public $id;
+    public $name;
+}
