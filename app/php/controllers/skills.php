8d0cc2c36407237ba0805b8cd02add50c50c1654
marian radu <doradu@pentalog.fr>
2014-06-07 13:42:13 +0300

    addSkill

--- app/php/controllers/skills.php
+++ app/php/controllers/skills.php
@@ -25,6 +25,17 @@ class skillsController
         $userId = $_REQUEST['user_id'];
         $skillId = $_REQUEST['skill_id'];
 
+        $sql = "INSERT INTO skillrelation (user_id,skill_id) VALUES (:user_id,:skill_id)";
+        $st = $this->database->prepare($sql);
+        $st->execute(array(':user_id' => $_REQUEST['user_id'],':skill_id' => $_REQUEST['skill_id']));
+
+        echo json_encode(
+            array(
+                'id' => $this->database->lastInsertId()
+                ,'user_id' => $_REQUEST['user_id']
+                ,'skill_id' => $_REQUEST['skill_id']
+            )
+        );
 
     }
 
